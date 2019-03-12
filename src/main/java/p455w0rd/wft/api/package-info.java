897472bd873dbde1d4684427ac5a766126c9ad5b
p455w0rd <p455w0rd@gmail.com>
2019-03-12 13:39:20 -0400

    couple small fixes

--- src/main/java/p455w0rd/wft/api/package-info.java
+++ src/main/java/p455w0rd/wft/api/package-info.java
@@ -1,4 +1,4 @@
-@API(apiVersion = "1.1", owner = "wct", provides = "wct|api")
+@API(apiVersion = p455w0rd.wft.init.ModGlobals.VERSION, owner = "wft", provides = "wft|api")
 package p455w0rd.wft.api;
 
 import net.minecraftforge.fml.common.API;
